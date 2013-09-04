911a3a4a70cf9a43593bdea3ae66f109081f54e3
carlos arboleda <carlosarboleda@MacBook-Pro-de-carlos-arboleda.local>
2013-09-03 21:09:42 -0500

    parcial 1

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Miapppost::Application.config.secret_token = '4d89e04810ee05d0581d53bf0b1eaea4280530f7d85b442024fec1d0b36ec773b3d34254b2a0bc87bf1e7267bf181bd349e66062f9f9908b035a61f4fd8c57a6'
